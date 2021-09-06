4d1919cd504c672751810d3ea6f65aa7803b78bc
prabhakar <pp19abj@herts.ac.uk>
2021-09-06 18:40:21 +0100

    creating instance using resorce of aws instance

--- 03-example1/ec2.tf
+++ 03-example1/ec2.tf
@@ -1,26 +1,26 @@
 resource "aws_security_group" "allow_ssh" {
-  name        = "allow_ssh"
-  description = ""
+  name                = "allow_ssh"
+  description         = ""
 
 
-  ingress = [
+  ingress =
     {
-      description      = "ssh"
-      from_port        = 22
-      to_port          = 22
-      protocol         = "tcp"
-      cidr_blocks      = ["0.0.0.0/0"]
+      description     = "ssh"
+      from_port       = 22
+      to_port         = 22
+      protocol        = "tcp"
+      cidr_blocks     = ["0.0.0.0/0"]
     }
-  ]
 
-  egress = [
+
+  egress =
     {
       from_port        = 0
       to_port          = 0
       protocol         = "-1"
       cidr_blocks      = ["0.0.0.0/0"]
     }
-  ]
+
 
   tags                 = {
     Name               = "allow_tls"
