8839a4bc5dbf6d3b33424353d5594a69a196bc6e
prabhakar <pp19abj@herts.ac.uk>
2021-09-06 19:03:42 +0100

    creating instance using resorce of aws instance

--- 02-variable1/terraform.tfvars
+++ 02-variable1/terraform.tfvars
@@ -1,10 +1,2 @@
 FRUITS=["APPLE","FRUITS"]
 
-resource "aws_instance" "sample" {
-  ami               = "ami-074df373d6bafa625"
-  instance_type     = "t3.micro"
-  vpc_security_group_ids = []
-  tags              = {
-    Name            = "sample"
-  }
-}
--- 03-example1/ec2.tf
+++ 03-example1/ec2.tf
@@ -1,3 +1,11 @@
+resource "aws_instance" "sample" {
+  ami               = "ami-074df373d6bafa625"
+  instance_type     = "t3.micro"
+  vpc_security_group_ids = [aws_security_group.allow_ssh.id]
+  tags              = {
+    Name            = "sample"
+  }
+}
 resource "aws_security_group" "allow_ssh" {
   name                = "allow_ssh"
   description         = ""
@@ -24,8 +32,8 @@ resource "aws_security_group" "allow_ssh" {
     Name               = "allow_tls"
   }
 }
-output "sg-attributes" {
-  value = aws_security_group.allow_ssh
+output "ec2-attriutes" {
+  value = aws_instance.sample
 }
 
 provider "aws"{
